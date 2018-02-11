0049e4ac06cac92f6c56c3410a2286bc68855b91
Kaspar RAVEL <kaspar.ravel@gmail.com>
2018-02-11 17:23:35 +0100

    Update moshnbake.py

--- moshnbake.py
+++ moshnbake.py
@@ -1,15 +1,16 @@
 #!/bin/python3 
-from time import sleep
 import os
+from os.path import basename
 
-i = 1
 
 for filename in os.listdir("."):
-	stri = str(i) 
-	os.system("ffmpeg -i " + filename + " -c libxvid -qscale:v 1 -an -bf 0 -g 400 bad1" + stri + ".avi")
-	os.system("python tomato.py -i bad1" + stri + ".avi -m pulse -c 2 -n 1 bad2" + stri + ".avi" )
-	#os.system("mencoder -forceidx -oac copy -ovc copy badB" + stri + ".avi -o bad2" + stri + ".avi")
-	os.system("mencoder bad2" + stri + ".avi -o bad3" + stri + ".mov -ovc x264")
-	os.system("ffmpeg -i bad3" + stri + ".mov -c:v prores_ks final" + stri + ".mov")
-	os.system("del bad*")
-	i = i + 1
+	os.system("ffmpeg -i " + filename + " -c:v libxvid -qscale:v 1 -an -bf 0 -g 900 ENC" + os.path.splitext(filename)[0] + ".avi")
+for filename in os.listdir("."):
+	if "ENC" in filename:
+		os.system("python tomato.py -i " + filename + " -m pulse -c 15 -n 1 OUT" + os.path.splitext(filename)[0] + ".avi")
+for filename in os.listdir("."):
+	if "OUT" in filename:
+		os.system("mencoder " + filename + " -o OUT2" + os.path.splitext(filename)[0] + ".mov -ovc x264")
+for filename in os.listdir("."):
+	if "OUT2" in filename:
+		os.system("ffmpeg -i " + filename + " -c:v prores_ks BAKED" + os.path.splitext(filename)[0] + ".mov")
