a2f32c08b54328c62d28d2e5b5f0c4bb96060260
Kaspar RAVEL <kaspar.ravel@gmail.com>
2018-02-11 17:45:53 +0100

    Update batchbake.py

--- batchbake.py
+++ batchbake.py
@@ -2,10 +2,18 @@
 import os
 from os.path import basename
 
-for filename in os.listdir("."):
-	if "OUT" in filename:
-		os.system("mencoder " + filename + " -o OUT2" + os.path.splitext(filename)[0] + ".mov -ovc x264")
+if not os.path.exists("baked"):
+	os.makedirs("baked")
+
+if not os.path.exists("temp"):
+	os.makedirs("temp")
 
 for filename in os.listdir("."):
-	if "OUT2" in filename:
-		os.system("ffmpeg -i " + filename + " -c:v prores_ks BAKED" + os.path.splitext(filename)[0] + ".mov")
+	if os.path.isdir(filename):
+		continue
+	os.system("mencoder " + filename + " -nosound -o temp/" + os.path.splitext(filename)[0] + ".mov -ovc x264")
+
+for filename in os.listdir("temp"):
+	if os.path.isdir(filename):
+		continue
+	os.system("ffmpeg -i temp/" + filename + " -c:v prores_ks baked/" + os.path.splitext(filename)[0] + ".mov")
