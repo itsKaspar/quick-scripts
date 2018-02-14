532f593831976dd49c438d7bbbf2a982f25274ea
Kaspar RAVEL <kaspar.ravel@gmail.com>
2018-02-14 12:42:43 +0100

    Update sonia.py

--- sonia.py
+++ sonia.py
@@ -6,7 +6,7 @@ from moviepy.editor import VideoFileClip
 import re
 
 g = 600 # nb of seconds that we strip from beginning and end of movie to avoid credits
-j = 50 # nb of cuts you want
+j = 5 # nb of cuts you want
 i = 0
 
 
@@ -17,7 +17,7 @@ if not os.path.exists("results"):
 
 while i < j :
 	for filename in os.listdir("."):
-		if os.path.isdir(filename):
+		if os.path.isdir(filename) or filename == "sonia.py":
 	      		continue
 			
 		clip = VideoFileClip(filename)
@@ -27,7 +27,11 @@ while i < j :
 		max = int(film_duration) - g - 180
 		
   		k = random.randint(min, max)
-		os.system("ffmpeg -ss " + str(k) +  " -i " + filename + " -t 180 -c:v libx264 results/" + os.path.splitext(filename)[0] + ".mp4")
+		os.system("ffmpeg -ss " + str(k) +  " -i " + filename + " -t 10 -c:v libx264 results/" + str(i) + ".mp4")
+		
+		print "################################################"
+		print "made cut from " + str(k) + "sec to " + str(k + 180) + "sec " 
+		print "################################################"
 		
 		i += 1
 	
