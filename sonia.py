ea03b6d026d4f0648858fe9bbf58ffd0a318e526
Kaspar RAVEL <kaspar.ravel@gmail.com>
2018-02-14 12:04:47 +0100

    Create sonia.py

---
+++ sonia.py
@@ -0,0 +1,35 @@
+#!/bin/python3  
+
+import os
+import random
+import subprocess
+import re
+
+g = 600 # nb of seconds that we strip from beginning and end of movie to avoid credits
+
+if not os.path.exists("results"): 
+	os.makedirs("results")
+
+while i < 500 :
+  for filename in os.listdir("."):
+    if os.path.isdir(filename):
+	      continue
+ 
+    process = subprocess.Popen(['/usr/bin/ffmpeg',  '-i', PATH_TO_YOUR_VIDEO_FILE], stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+    stdout, stderr = process.communicate()
+    matches = re.search(r"Duration:\s{1}(?P\d+?):(?P\d+?):(?P\d+\.\d+?),", stdout, re.DOTALL).groupdict()
+ 
+   film_duration = matches['minutes']
+  k = random.randint(g, film_duration-g-180)
+	os.system("ffmpeg -ss " + k +  " -i " + filename + " -t 180 -c:v libx264 results/" + os.path.splitext(filename)[0] + ".mp4")
+
+  i++
+
+
+
+
+  
+  
+
+
+	
