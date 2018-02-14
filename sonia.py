25cd5fc65235e0ab00079ade0ecbf9ca6a1d8c52
Kaspar RAVEL <kaspar.ravel@gmail.com>
2018-02-14 12:26:39 +0100

    Update sonia.py

--- sonia.py
+++ sonia.py
@@ -2,12 +2,15 @@
 
 import os
 import random
-import subprocess
+from moviepy.editor import VideoFileClip
 import re
 
 g = 600 # nb of seconds that we strip from beginning and end of movie to avoid credits
 j = 50 # nb of cuts you want
 i = 0
+
+
+
 
 if not os.path.exists("results"): 
 	os.makedirs("results")
@@ -17,10 +20,8 @@ while i < j :
 		if os.path.isdir(filename):
 	      		continue
 			
-		process = subprocess.Popen(['/usr/bin/ffmpeg',  '-i', filename], stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
-    		stdout, stderr = process.communicate()
-    		matches = re.search(r"Duration:\s{1}(?P\d+?):(?P\d+?):(?P\d+\.\d+?),", stdout, re.DOTALL).groupdict()
-   		film_duration = matches['minutes']
+		clip = VideoFileClip(filename)
+		film_duration = clip.duration
 		
   		k = random.randint(g, film_duration-g-180)
 		os.system("ffmpeg -ss " + k +  " -i " + filename + " -t 180 -c:v libx264 results/" + os.path.splitext(filename)[0] + ".mp4")
