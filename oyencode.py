648554fa1ac864668b3a3dda1ee2b3277dc41e28
Kaspar RAVEL <kaspar.ravel@gmail.com>
2018-02-15 12:06:44 +0100

    Update oyencode.py

--- oyencode.py
+++ oyencode.py
@@ -1,21 +1,23 @@
 #!/usr/bin/python2.7
 
 import os
+from os.path import basename
 
-if not os.path.exists("encoded"): 
-	os.makedirs("encoded")
+d = "encoded" 	# directory name
+enc = "libx264"	# encoder
+ext = ".mp4"	# extension
+enc_opt = ""	# encoder options
+
+# "-movflags +faststart"  if it is a webvideo and needs to be played by user on browser before it finished downloading
+# "-tune film" // or animation
+
+if not os.path.exists(d): 
+	os.makedirs(d)
 
 	for filename in os.listdir("."):
-		if os.path.isdir(filename) or filename == "sonia.py":
+		if os.path.isdir(f):
 	      		continue
 			
-		clip = VideoFileClip(filename)
-		film_duration = clip.duration
-		
-		min = g
-		max = int(film_duration) - g - t
-		
-  		k = random.randint(min, max)
-		os.system("ffmpeg -ss " + str(k) +  " -i " + filename + " -t " + str(t) + " -c:v libx264 results/" + str(i) + ".mp4")
-		
-		i += 1
+		fnoext = os.path.splitext(f)[0]
+		output = d + "/" + fnoext + ext
+		os.system("ffmpeg -i " + f + " -c:v " + enc + " " + enc_opt + " " + output)
