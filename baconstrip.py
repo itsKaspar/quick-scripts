07f34ae0a6a023c0f72b2c56d6e4726e5c4b9611
Kaspar RAVEL <kaspar.ravel@gmail.com>
2018-03-05 23:28:46 +0100

    Update baconstrip.py

--- baconstrip.py
+++ baconstrip.py
@@ -2,18 +2,10 @@
 import os
 from os.path import basename
 
-if not os.path.exists("baked"):
-	os.makedirs("baked")
-
-if not os.path.exists("temp"): 
-	os.makedirs("temp")
+if not os.path.exists("bacon"):
+	os.makedirs("bacon")
 
 for filename in os.listdir("."):
 	if os.path.isdir(filename):
 		continue
-	os.system("mencoder " + filename + " -o temp/" + os.path.splitext(filename)[0] + ".mov -ovc x264 -oac lavc")
-
-for filename in os.listdir("temp"):
-	if os.path.isdir(filename):
-		continue
-	os.system("ffmpeg -i temp/" + filename + " -c:v libx264 -crf 0 -preset ultrafast baked/" + os.path.splitext(filename)[0] + ".mov")
+	os.system("ffmpeg -i " + filename + " -c:v libxvid -crf 0 -preset ultrafast -c:a libmp3lame encoded/" + os.path.splitext(filename)[0] + ".mkv")
